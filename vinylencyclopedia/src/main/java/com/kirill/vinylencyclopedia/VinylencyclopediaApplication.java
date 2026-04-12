13cc86ddfb5902b610a239f9b9c7db24548ed7ed
rakhimichi <kirill.rakhimov@myy.haaga-helia.fi>
2026-04-12 14:05:16 +0300

    Initial Spring Boot project setup

---
+++ vinylencyclopedia/src/main/java/com/kirill/vinylencyclopedia/VinylencyclopediaApplication.java
@@ -0,0 +1,13 @@
+package com.kirill.vinylencyclopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylencyclopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylencyclopediaApplication.class, args);
+	}
+
+}
---
+++ vinylencyclopedia/src/test/java/com/kirill/vinylencyclopedia/VinylencyclopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirill.vinylencyclopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylencyclopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
